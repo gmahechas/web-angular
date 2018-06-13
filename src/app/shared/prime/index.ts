fb2b8103035dbfd22397464ada60585c6dba3dca
gmahechas <gmahechas@ingenieros.com>
2018-06-12 20:16:36 -0500

    Fixing Prime Modules
    Fixing prime modules imports

---
+++ src/app/shared/prime/index.ts
@@ -0,0 +1,29 @@
+import { SidebarModule } from 'primeng/sidebar';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
+import { InputTextModule } from 'primeng/inputtext';
+import { GrowlModule } from 'primeng/growl';
+import { BlockUIModule } from 'primeng/blockui';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { ProgressBarModule } from 'primeng/progressbar';
+
+export const prime: any[] = [
+  SidebarModule,
+  PanelMenuModule,
+  ButtonModule,
+  TableModule,
+  PaginatorModule,
+  InputTextModule,
+  GrowlModule,
+  BlockUIModule,
+  ProgressSpinnerModule,
+  ConfirmDialogModule,
+  MessagesModule,
+  MessageModule,
+  ProgressBarModule
+];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,41 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { SidebarModule } from 'primeng/sidebar';
-import { PanelMenuModule } from 'primeng/panelmenu';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { PaginatorModule } from 'primeng/paginator';
-import { InputTextModule } from 'primeng/inputtext';
-import { TooltipModule } from 'primeng/tooltip';
-import { GrowlModule } from 'primeng/growl';
-import { BlockUIModule } from 'primeng/blockui';
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { MessagesModule } from 'primeng/messages';
-import { MessageModule } from 'primeng/message';
-import { ProgressBarModule } from 'primeng/progressbar';
-
+import * as fromPrime from './prime';
 import * as fromComponents from './components';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    SidebarModule,
-    PanelMenuModule,
-    ButtonModule,
-    TableModule,
-    PaginatorModule,
-    InputTextModule,
-    TooltipModule,
-    GrowlModule,
-    BlockUIModule,
-    ProgressSpinnerModule,
-    ConfirmDialogModule,
-    MessagesModule,
-    MessageModule,
-    ProgressBarModule
+    ...fromPrime.prime
   ],
   declarations: [
     ...fromComponents.components
@@ -45,20 +18,7 @@ import * as fromComponents from './components';
   exports: [
     CommonModule,
     ReactiveFormsModule,
-    SidebarModule,
-    PanelMenuModule,
-    ButtonModule,
-    TableModule,
-    PaginatorModule,
-    InputTextModule,
-    TooltipModule,
-    GrowlModule,
-    BlockUIModule,
-    ProgressSpinnerModule,
-    ConfirmDialogModule,
-    MessagesModule,
-    MessageModule,
-    ProgressBarModule,
+    ...fromPrime.prime,
     ...fromComponents.components
   ]
 })
