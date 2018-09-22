7d609340f522ce5e00bf9c2938621aaf66cd6948
gmahechas <gmahechas@ingenieros.com>
2018-09-22 12:32:26 -0500

    update graphql module

--- src/app/graphql/graphql.module.ts
+++ src/app/graphql/graphql.module.ts
@@ -1,68 +1,49 @@
 import { NgModule } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
 
-import { Store } from '@ngrx/store';
-import * as fromCore from '../core/store';
-import * as fromAuthActions from '../auth/store/actions';
-
-import { ApolloModule, Apollo } from 'apollo-angular';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
-import { ApolloLink, from } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink, from } from 'apollo-link';
 import { onError } from 'apollo-link-error';
 
 import { environment } from './../../environments/environment';
 
-@NgModule({
-  imports: [
-    ApolloModule,
-    HttpLinkModule
-  ],
-  declarations: []
-})
-export class GraphqlModule {
-  constructor(
-    private apollo: Apollo,
-    private httpLink: HttpLink,
-    private store: Store<fromCore.State>
-  ) {
-    const link = httpLink.create({
-      uri: environment.apilUrl + environment.graphqlUrl,
-      method: 'POST'
-    });
+const tokenMiddleware = new ApolloLink((operation, forward) => {
+  // console.log(operation);
+  return forward(operation);
+});
 
-    const cache = new InMemoryCache({
+const errorMiddleware = onError(({ graphQLErrors, networkError, operation, forward }) => {
+  // console.log('Error:::', { graphQLErrors, networkError, operation, forward });
+});
+
+export function createApollo(httpLink: HttpLink) {
+  const link = httpLink.create({ uri: environment.apilUrl + environment.graphqlUrl });
+  return {
+    link: from([
+      tokenMiddleware,
+      errorMiddleware,
+      link
+    ]),
+    cache: new InMemoryCache({
       addTypename: false
-    });
-
-    const tokenMiddleware = new ApolloLink((operation, forward) => {
-      const token = JSON.parse(localStorage.getItem('token'));
-      operation.setContext({
-        headers: new HttpHeaders().set('Authorization', token.token_type + ' ' + token.access_token || null)
-      });
-      return forward(operation);
-    });
-
-    const errorMiddleware = onError(({ graphQLErrors, networkError, operation, forward }) => {
-      const token = JSON.parse(localStorage.getItem('token'));
-      console.log('old->', token);
-
-      if (networkError['status'] === 401) {
-        this.store.dispatch(new fromAuthActions.RefreshToken(token));
+    }),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: 'network-only'
       }
-
-      const newToken = JSON.parse(localStorage.getItem('token'));
-      console.log('new->', newToken);
-    });
-
-    apollo.create({
-      link: from([/* errorMiddleware, tokenMiddleware,  */link]),
-      cache,
-      defaultOptions: {
-        watchQuery: {
-          fetchPolicy: 'network-only'
-        }
-      }
-    });
-  }
+    }
+  };
 }
+
+@NgModule({
+  exports: [ApolloModule, HttpLinkModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ]
+})
+export class GraphqlModule { }
