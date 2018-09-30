cdda3ddcb13fb4d52e7b4b3d4ffa36a03425f7d2
gmahechas <gmahechas@ingenieros.com>
2018-09-29 19:17:45 -0500

    Fix recall for suscription in e4570d5 :fire: :fire: :muscle:

--- src/app/two/profile-menu/containers/index-page-profile-menu/index-page-profile-menu.component.ts
+++ src/app/two/profile-menu/containers/index-page-profile-menu/index-page-profile-menu.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { Store, select } from '@ngrx/store';
 import * as fromStore from './../../store';
-import * as fromProfile from '../../../profile/store';
-
-import { Profile } from '../../../profile/models';
-
-import { Subscription } from 'rxjs';
-import { filter } from 'rxjs/operators';
-
 
 @Component({
   selector: 'app-index-page-profile-menu',
@@ -16,34 +10,22 @@ import { filter } from 'rxjs/operators';
   templateUrl: './index-page-profile-menu.component.html',
   styles: []
 })
-export class IndexPageProfileMenuComponent implements OnInit, OnDestroy {
+export class IndexPageProfileMenuComponent implements OnInit {
 
-  subscription: Subscription;
   data$ = this.store.pipe(select(fromStore.getAllEntities));
 
   constructor(
-    private store: Store<fromStore.State>
+    private store: Store<fromStore.State>,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
-
-    this.subscription = this.store.pipe(
-      select(fromProfile.getSelectedByRouter),
-      filter(entity => entity !== undefined)
-    ).subscribe(
-      (profile: Profile) => {
-        this.store.dispatch(new fromStore.LoadEntity({
-          search: {
-            profile: {
-              profile_id: String(profile.profile_id)
-            }
-          }
-        }));
+    this.store.dispatch(new fromStore.LoadEntity({
+      search: {
+        profile: {
+          profile_id: this.route.snapshot.params.profile_id
+        }
       }
-    );
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+    }));
   }
 }
