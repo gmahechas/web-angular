0352935c8688b092be67cfac63b21efc81e2651d
gmahechas <gmahechas@ingenieros.com>
2018-10-09 18:07:12 -0500

    nested array parent

--- src/app/shared/pipes/parent-child-array-shared.pipe.ts
+++ src/app/shared/pipes/parent-child-array-shared.pipe.ts
@@ -1,12 +1,37 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import { TreeNode } from 'primeng/api';
+
 @Pipe({
   name: 'parentChildArrayShared'
 })
 export class ParentChildArraySharedPipe implements PipeTransform {
 
-  transform(value: any): any {
-    return value;
+  transform(objects: any): any {
+
+    const newObjects: TreeNode[] = [];
+
+    objects.map(object => {
+      newObjects.push({
+        label: object.menu.menu_name,
+        data: object
+      });
+    });
+    const end = this.nested(newObjects);
+    return end;
   }
 
+  nested(objects, parent = null) {
+    const out = [];
+    objects.forEach(element => {
+      if (element.data.menu.menu_parent_id === parent) {
+        const children = this.nested(objects, element.data.menu.menu_id);
+        if (children.length) {
+          element.children.push(children);
+        }
+        out.push(element);
+      }
+    });
+    return out;
+  }
 }
