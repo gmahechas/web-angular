45531f484fdaebe94e763564a3f512b1131625c2
gmahechas <gmahechas@ingenieros.com>
2019-01-07 20:57:38 -0500

    fix ParentChildArraySharedPipe with args
    close #36

--- src/app/shared/pipes/parent-child-array-shared.pipe.ts
+++ src/app/shared/pipes/parent-child-array-shared.pipe.ts
@@ -1,30 +1,33 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import { get } from 'lodash';
+
 @Pipe({
   name: 'parentChildArrayShared'
 })
 export class ParentChildArraySharedPipe implements PipeTransform {
 
-  transform(objects: any): any {
+  transform(objects: any, id: any, parent_id: any, label?: any): any {
 
     const newObjects: any[] = [];
 
     objects.map(object => {
       newObjects.push({
-        label: object.menu.menu_name,
+        label: get(object, label, label),
         data: object,
         children: []
       });
     });
 
-    return this.nested(newObjects);
+    console.log(newObjects);
+    return this.nested(newObjects, id, parent_id);
   }
 
-  nested(objects, parentId = null) {
+  nested(objects, id: any, parent_id: any, parentId = null) {
     const out = [];
     objects.forEach(element => {
-      if (element.data.menu.menu_parent_id === parentId) {
-        const children = this.nested(objects, element.data.menu.menu_id);
+      if (get(element.data, parent_id, parent_id) === parentId) {
+        const children = this.nested(objects, id, parent_id, get(element.data, id, id));
         if (children.length) {
           children.forEach(child => {
             element.children.push(child);
