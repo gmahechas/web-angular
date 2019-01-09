2b16145506a6dc53c13e6cd0301528a302a34184
gmahechas <gmahechas@ingenieros.com>
2019-01-09 16:00:55 -0500

    add selected in bar menu

--- src/app/core/components/bar-menu-core/bar-menu-core.component.ts
+++ src/app/core/components/bar-menu-core/bar-menu-core.component.ts
@@ -6,17 +6,24 @@ import { ParentChild } from '@web/app/core/models/parent-child.model';
   selector: 'app-bar-menu-core',
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './bar-menu-core.component.html',
-  styles: []
+  styles: [`
+    .selected_tim {
+      background-color: #007ad9;
+      color: #ffffff;
+    }
+  `]
 })
 export class BarMenuCoreComponent implements OnInit {
 
   @Input() bar: number;
   @Input() options: ParentChild[];
+  @Input() selected: number;
   @Output() navigate: EventEmitter<{ bar: number, option: ParentChild }> = new EventEmitter<{ bar: number, option: ParentChild }>();
 
   constructor() { }
 
   ngOnInit() {
+    /*     console.log(this.selected); */
   }
 
   handleNavigate(option: ParentChild) {
--- src/app/core/components/menu-core/menu-core.component.ts
+++ src/app/core/components/menu-core/menu-core.component.ts
@@ -11,22 +11,27 @@ import { ParentChild } from '@web/app/core/models/parent-child.model';
 })
 export class MenuCoreComponent implements OnInit {
 
-  bars: { bar: number, options: ParentChild[] }[] = [];
+  bars: { bar: number, selected: number | null, options: ParentChild[] }[] = [];
   @Input() options: ParentChild[];
   @Output() navigate: EventEmitter<ProfileMenu> = new EventEmitter<ProfileMenu>();
 
   constructor() { }
 
   ngOnInit() {
-    this.bars.push({ bar: 0, options: this.options });
+    this.bars.push({ bar: 0, selected: null, options: this.options });
   }
 
   onNavigate(option: { bar: number, option: ParentChild }) {
     if (option.option.data.menu.menu_uri) {
+      if (option.option.data.profile_menu_id === '1') {
+        this.bars.splice((option.bar + 1), this.bars.length);
+      }
+      this.bars[option.bar].selected = option.option.data.profile_menu_id;
       this.navigate.emit(option.option.data);
     } else {
       this.bars.splice((option.bar + 1), this.bars.length);
-      this.bars.push({ bar: this.bars.length, options: option.option.children });
+      this.bars[option.bar].selected = option.option.data.profile_menu_id;
+      this.bars.push({ bar: this.bars.length, selected: null, options: option.option.children });
     }
   }
 }
