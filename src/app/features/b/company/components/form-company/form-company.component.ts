4fa89d39c52e29df6d8c0bffc838575ce4e41989
gmahechas <gmahechas@ingenieros.com>
2019-01-18 16:13:53 -0500

    fix changeDetection in components

--- src/app/features/b/company/components/form-company/form-company.component.ts
+++ src/app/features/b/company/components/form-company/form-company.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-form-company',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './form-company.component.html',
   styles: []
 })
