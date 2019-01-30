e4246a66395bba1001cdd627ccd6bbb864b7fc21
gmahechas <gmahechas@ingenieros.com>
2019-01-30 17:41:16 -0500

    add CREATE FORM in OFFICE DEPARTMENT

---
+++ src/app/features/b/office-department/components/create-form-office-department/create-form-office-department.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-create-form-office-department',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './create-form-office-department.component.html',
+  styles: []
+})
+export class CreateFormOfficeDepartmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/features/b/office-department/components/index.ts
+++ src/app/features/b/office-department/components/index.ts
@@ -1,7 +1,11 @@
 import {
   EditFormOfficeDepartmentComponent
 } from '@web/app/features/b/office-department/components/edit-form-office-department/edit-form-office-department.component';
+import {
+  CreateFormOfficeDepartmentComponent
+} from '@web/app/features/b/office-department/components/create-form-office-department/create-form-office-department.component';
 
 export const components: any[] = [
-  EditFormOfficeDepartmentComponent
+  EditFormOfficeDepartmentComponent,
+  CreateFormOfficeDepartmentComponent
 ];
