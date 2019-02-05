2b350b79f2701115506d8cac3813aa0b2d3a2855
gmahechas <gmahechas@ingenieros.com>
2019-02-05 10:56:35 -0500

    fix order

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,8 +4,8 @@ import { StoreModule } from '@ngrx/store';
 import { reducers, effects } from '@web/app/auth/store';
 import { EffectsModule } from '@ngrx/effects';
 
-import { AuthRoutingModule } from '@web/app/auth/auth-routing.module';
 import { SharedModule } from '@web/app/shared/shared.module';
+import { AuthRoutingModule } from '@web/app/auth/auth-routing.module';
 
 import * as fromContainers from '@web/app/auth/containers';
 import * as fromComponents from '@web/app/auth/components';
