bdba93baedc0221a9bcefe8e934362ff0940ba6f
gmahechas <gmahechas@ingenieros.com>
2019-02-05 19:38:55 -0500

    MOVE ALL STORE FROM APP MODULE TO CORE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { reducers, effects, metaReducers } from '@web/app/core/store';
-import { StoreRouterConnectingModule, RouterStateSerializer } from '@ngrx/router-store';
-import { CustomPreload, CustomRouterStateSerializer } from '@web/app/shared/router-utils';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
 import { AppLangModule } from '@web/app/app-lang.module';
 import { AppGraphqlModule } from '@web/app/app-graphql.module';
 import { SharedModule } from '@web/app/shared/shared.module';
 import { AuthModule } from '@web/app/auth/auth.module';
 import { AppRoutingModule } from '@web/app/app-routing.module';
 import { CoreModule } from '@web/app/core/core.module';
-
-import { httpInterceptorProviders } from '@web/app/core/interceptors/index';
-
-import { environment } from '@web/environments/environment';
 
 import { IndexPageCoreComponent } from '@web/app/core/containers/index-page-core/index-page-core.component';
 
@@ -33,22 +22,7 @@ import { IndexPageCoreComponent } from '@web/app/core/containers/index-page-core
     SharedModule,
     AuthModule,
     AppRoutingModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
-    StoreRouterConnectingModule.forRoot({
-      stateKey: 'router',
-    }),
-    StoreDevtoolsModule.instrument({
-      name: 'NgRx Web Store DevTools',
-      maxAge: 25,
-      logOnly: environment.production
-    }),
-    EffectsModule.forRoot(effects),
     CoreModule.forRoot()
-  ],
-  providers: [
-    CustomPreload,
-    { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer },
-    httpInterceptorProviders
   ],
   bootstrap: [IndexPageCoreComponent]
 })
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { reducers, effects, metaReducers } from '@web/app/core/store';
+import { StoreRouterConnectingModule, RouterStateSerializer } from '@ngrx/router-store';
+import { CustomPreload, CustomRouterStateSerializer } from '@web/app/shared/router-utils';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { SharedModule } from '@web/app/shared/shared.module';
 
@@ -7,10 +13,23 @@ import * as fromContainers from '@web/app/core/containers';
 import * as fromComponents from '@web/app/core/components';
 import * as fromServices from '@web/app/core/services';
 
+import { httpInterceptorProviders } from '@web/app/core/interceptors/index';
+
+import { environment } from '@web/environments/environment';
+
 @NgModule({
   imports: [
     SharedModule,
-    RouterModule
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router',
+    }),
+    StoreDevtoolsModule.instrument({
+      name: 'NgRx Web Store DevTools',
+      maxAge: 25,
+      logOnly: environment.production
+    }),
+    EffectsModule.forRoot(effects),
   ],
   declarations: [
     ...fromContainers.containers,
@@ -26,7 +45,10 @@ export class CoreModule {
     return {
       ngModule: CoreModule,
       providers: [
-        ...fromServices.services
+        CustomPreload,
+        { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer },
+        httpInterceptorProviders,
+        ...fromServices.services,
       ],
     };
   }
