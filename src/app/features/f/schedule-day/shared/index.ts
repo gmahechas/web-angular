f9dd0a8f319f6588e01826090f65404cc27fde1a
gmahechas <gmahechas@ingenieros.com>
2019-02-19 12:20:57 -0500

    ng lint

--- src/app/features/f/schedule-day/shared/index.ts
+++ src/app/features/f/schedule-day/shared/index.ts
@@ -1,4 +1,6 @@
-import { DropdownPageScheduleDayComponent } from '@web/app/features/f/schedule-day/shared/dropdown-page-schedule-day/dropdown-page-schedule-day.component';
+import {
+  DropdownPageScheduleDayComponent
+} from '@web/app/features/f/schedule-day/shared/dropdown-page-schedule-day/dropdown-page-schedule-day.component';
 
 export const shared: any[] = [
   DropdownPageScheduleDayComponent
