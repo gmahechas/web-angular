ea0ed7b0f32d1562174b804c29b36d14213e694e
gmahechas <gmahechas@ingenieros.com>
2019-03-07 18:42:44 -0500

    Update menu.model.ts

--- src/app/features/c/menu/models/menu.model.ts
+++ src/app/features/c/menu/models/menu.model.ts
@@ -5,7 +5,7 @@ export interface Menu {
   menu_upper_case?: boolean;
   menu_uri?: string;
   menu_parent_id?: number;
-  menu_parent?: Menu;
+  menu_parent?: Menu | null;
   menu_created_at?: string;
   menu_updated_at?: string;
   menu_deleted_at?: string;
