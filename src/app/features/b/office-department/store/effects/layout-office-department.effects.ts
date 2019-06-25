cce0d54e2f5f89a8b8b1b5d829902b8777480285
gmahechas <gmahechas@ingenieros.com>
2019-06-25 11:13:37 -0500

    Update layout-office-department.effects.ts

--- src/app/features/b/office-department/store/effects/layout-office-department.effects.ts
+++ src/app/features/b/office-department/store/effects/layout-office-department.effects.ts
@@ -79,7 +79,7 @@ export class LayoutOfficeDepartmentEffects {
     { dispatch: false }
   );
 
-  // Redirects
+/*   // Redirects
   successRedirect$ = createEffect(() =>
     this.actions$.pipe(
       ofType(
@@ -105,7 +105,7 @@ export class LayoutOfficeDepartmentEffects {
       })
     ),
     { dispatch: false }
-  );
+  ); */
 
   constructor(
     private actions$: Actions,
