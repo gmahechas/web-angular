2c33662788b5c73bca9db03ad9bee168e8910373
gmahechas <gmahechas@ingenieros.com>
2021-01-05 21:21:42 -0500

    setup in local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'http://local.dev.co/laravel/api-laravel/public/api/',
+  api: 'http://local.dev.co:8000/api/',
   login: 'auth/login',
   refresh: 'auth/refresh',
   graphql: 'graphql'
